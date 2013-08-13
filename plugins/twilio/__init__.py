6e85b92aa1809938d39a390b357ed03c9de7e91f
Daigo Tanaka <daigo.tanaka@gmail.com>
2013-08-12 19:13:04 -0700

    Added more voice commands for sending and reading out sms

--- plugins/twilio/__init__.py
+++ plugins/twilio/__init__.py
@@ -32,8 +32,8 @@ def register(app):
         return
     global twilio_plugin
     twilio_plugin = TwilioPlugin(app)
-    app.register_command(["send text to"], "send text", twilio_plugin.send_sms)
-    app.register_command(["read texts", "read text"], "read sms", twilio_plugin.read_out_sms)
+    app.register_command(["send an sms to", "send sms to", "send a text message to", "send a text to"], "send text", twilio_plugin.send_sms)
+    app.register_command(["read sms", "read text messages", "read text message", "read texts", "read text"], "read sms", twilio_plugin.read_out_sms)
 
 class TwilioPlugin(Plugin):
     def __init__(self, app):
