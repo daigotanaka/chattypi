12642fb36302a06f502a1eb153a09bd03c6210a0
Daigo Tanaka <daigo.tanaka@gmail.com>
2014-05-16 01:48:18 +0000

    Recover from xmpp disconnect

--- plugins/google_talk/__init__.py
+++ plugins/google_talk/__init__.py
@@ -45,10 +45,18 @@ class GoogleTalkPlugin(Plugin):
 
         self.last_from = None
         self.last_nickname = ""
-        self.gtalk_thread = Thread(target=self.gtalk_loop)
+        self.gtalk_thread = Thread(target=self.listen)
         self.gtalk_thread.start()
 
-    def gtalk_loop(self):
+    def listen(self):
+        while not self.app.exit_now:
+            try:
+                self.loop()
+            except AttributeError:
+                self.app.logger.debug("Attribute error. Recovering xmpp connection")
+                pass
+
+    def loop(self):
         user = """%s<script type="text/javascript">
         /* <![CDATA[ */
         (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName("script");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})();
@@ -60,10 +68,15 @@ class GoogleTalkPlugin(Plugin):
         domain = self.app.config.get("google_talk")["domain"]
         server = self.app.config.get("google_talk")["server"]
         port = self.app.config.get("google_talk")["port"]
+
+        self.app.logger.debug("Connecting to xmpp server")
         self.connection = xmpp.Client(domain, debug=[])
         self.connection.connect(server=(server, port))
+        self.app.logger.debug("xmpp connected to server")
         self.connection.auth(jid.getNode(), password, "LFY-client")
+        self.app.logger.debug("xmpp auth successful")
         self.connection.RegisterHandler("message", self.message_handler)
+        self.app.logger.debug("xmpp registered handler")
 
         self.connection.sendInitPresence()
 
